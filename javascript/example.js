003eec330c9420a876d9239896f23a3ac5e795f3
justinelugli <justinelugli@gmail.com>
2015-10-28 21:01:15 -0700

    my first commit

---
+++ javascript/example.js
@@ -0,0 +1,17 @@
+
+var someNumber = 12;
+var anotherNumber = someNumber + 4;
+console.log(someNumber);
+console.log(anotherNumber);
+var name = 'justine';
+var location = 'university of washington';
+console.log(name);
+console.log(location);
+
+var firstFavFood = 'pizza';
+var secondFavFood = 'ice cream';
+
+var favFoods = ['pizza', 'ice cream', 'turkey']
+
+console.log(favFoods[0]);
+console.log(favFoods[1]);
